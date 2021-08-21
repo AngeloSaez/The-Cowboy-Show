8fd75be637d00520cc973bd89f9d91caa296cee2
Angelo Saez <jello3899@gmail.com>
2021-08-21 12:24:09 -0400

    demo for dad

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     console.log('request event');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello world from the muffin man aka cat');
+    response.end('demonstration for dad');
 })
 
 server.on('connection', function() {
