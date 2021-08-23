9234b08cda2a0e7ae175005a94fd6d84bdce17d8
Angelo Saez <jello3899@gmail.com>
2021-08-23 18:19:53 -0400

    fs test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 
 server.on('request', function (request, response) {
     console.log('request event');
     response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
     response.end('demonstration for dad');
 })
 
