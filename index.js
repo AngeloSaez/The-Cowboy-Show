dec8f49f5f7546f52562c3c80135abc8ad79d2d3
Angelo Saez <jello3899@gmail.com>
2021-08-23 21:29:31 -0400

    html

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ var server = http.createServer();
 
 server.on('request', function (request, response) {
     console.log('request event');
-    
     fs.readFile('homepage.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        return response.end();
     });
-    
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('demonstration for dad');
-})
+});
 
 server.on('connection', function() {
     console.log('connection event');
