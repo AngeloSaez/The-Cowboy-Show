cba195155d536fb931ab2e1ef89bff6445f16db6
Angelo Saez <jello3899@gmail.com>
2021-08-25 18:30:26 -0400

    express version test

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-var port = process.env.PORT || 5000;
-var server = http.createServer();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-server.on('request', function (request, response) {
-    // Load CSS
-    if (request.url === '/styles/homepage.css') {
-        response.writeHead(200, {'Content-Type': 'text/css'});
-        fs.createReadStream('styles/homepage.css').pipe(response);
-    }
-    
-    // Load HTML
-    fs.readFile('html/homepage.html', function(err, data) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        return response.end();
-    });
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-
-server.on('connection', function() {
-    console.log('connection event');
-});
-
-server.listen(port, function() {
-    console.log('listen even');
+  
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
