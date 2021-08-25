040e19db9b618324d24a046770e2941247eec415
Angelo Saez <jello3899@gmail.com>
2021-08-25 03:53:18 -0400

    css

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var server = http.createServer();
 
 server.on('request', function (request, response) {
 
-    response.writeHead(200, {'Content-Type': 'image/png'});
-    fs.createReadStream('resources/cowboy_art.png').pipe(response);
-
     // Load CSS
     if (request.url === '/styles/homepage.css') {
         response.writeHead(200, {'Content-Type': 'text/css'});
