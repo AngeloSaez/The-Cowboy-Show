4b4f6d8bf57898bc6c3bd4602e5b8c2cefb84b81
Angelo Saez <jello3899@gmail.com>
2021-08-24 23:00:51 -0400

    css

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.on('request', function (request, response) {
     fs.readFile('styles/homepage.css', function(err, data) {
         response.writeHead(200, {'Content-Type': 'text/css'});
         response.write(data);
-        return response.end();
+        // return response.end();
     });
 
     fs.
