a86dd3ac5dd68fa27578366d18cbfffea085fcad
Angelo Saez <jello3899@gmail.com>
2021-08-24 21:54:15 -0400

    loaded css

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    // Load styles
-    // fs.readFile('styles/homepage.css');
-    // Load files
+    // Load CSS
+    fs.readFile('styles/homepage.css', function(err, data) {;
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        response.write(data);
+        return response.end();
+    });
+    
+    // Load HTML
     fs.readFile('html/homepage.html', function(err, data) {
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         return response.end();
     });
+
 });
 
 server.on('connection', function() {
