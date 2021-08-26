2802e56409f5a5d4adcc230c2c121d0dadec9d9e
Angelo Saez <jello3899@gmail.com>
2021-08-25 21:56:52 -0400

    corrected image name

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.get('/', (request, response) => {
   
 // Listening event
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Listening on port: ${port}`);
 });
