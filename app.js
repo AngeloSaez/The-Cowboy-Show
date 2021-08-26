13c50340d4f5861c90dc73c955acb6fcea3af300
Angelo Saez <jello3899@gmail.com>
2021-08-26 00:17:26 -0400

    trimmed out json

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var path = require('path');
-var indexRouter = require('./routes/index');
-
-var app = express();
+const app = express();
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+// express setup
+// app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
