26ec5fb9ee5214b0e293d164695f8a6a3566d84f
Angelo Saez <jello3899@gmail.com>
2021-08-26 00:11:21 -0400

    uncommented so style sheets worked

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'jade');
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
