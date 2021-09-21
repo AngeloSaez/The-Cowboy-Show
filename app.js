99eef7d4da1480dbe7af060352bb1b1cf9b219ef
Angelo Saez <jello3899@gmail.com>
2021-09-20 20:12:56 -0400

    moved routing code

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// router setup
-var router = express.Router();
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
 // express setup
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// router setup
+var router = express.Router();
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 app.use('/', router);
 
 // export
