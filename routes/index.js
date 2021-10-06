8f20c86ff19ae8c4b5de312bf85dc974ea97af0a
Angelo Saez <jello3899@gmail.com>
2021-10-05 21:36:49 -0400

    Added second page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Cowboy Show' });
+});
+
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { title: 'Gallery' });
 });
 
 module.exports = router;
