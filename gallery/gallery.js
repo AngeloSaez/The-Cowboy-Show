fc8ed9b2225b0186a74e1a2cf96aca03cb0f4bdb
Angelo Saez <jello3899@gmail.com>
2021-10-15 10:38:41 -0400

    Added p5js

---
+++ gallery/gallery.js
@@ -0,0 +1,92 @@
+////////////////////////////////////////////////////////////
+// State
+let current_state;
+
+////////////////////////////////////////////////////////////
+// Setup
+function preload() {
+  current_state = 0;
+  preload_gallery();
+}
+
+function setup() {
+  createCanvas(canvas_width, canvas_height, WEBGL);
+}
+
+////////////////////////////////////////////////////////////
+// Draw
+function draw() {
+  switch (current_state) {
+    case c_grid_state:
+      drawIntroState();
+      break;
+  }
+}
+
+////////////////////////////////////////////////////////////
+// Intro State
+function calculateMouseProximity(index) {
+  var cartridges_x = [];
+  var cartridges_y = hover_y;
+  var anchor_x = (-1 * total_width / 2.0) * entry_width;
+  var dx = Math.abs(anchor_x - mouseX);
+  var dy = Math.abs(hover_y - mouseY);
+  return Math.sqrt( dx * dx + dy * dy);
+}
+
+function calculateExhibitZ(mouse_prox) {
+  let max_z = 40;
+  let radius = box_size;
+  let ratio = min(radius, mouse_prox) / radius;
+  let result = ratio * max_z;
+  return result;
+}
+
+function drawIntroState() {
+  // Scenery
+  background(40);
+  noStroke();
+  ambientLight(ambient_color);
+  pointLight(100, 50, 108, 2 * box_size, 0, 10000);
+  // Exhibits
+  centerExhibits();
+  drawExhibits();
+}
+
+function centerExhibits() {
+  const translate_x = -1 * (total_width / 2.0);
+  translate(translate_x, 0, hover_y);
+}
+
+function drawExhibits() {
+  for (var i = 0; i < project_count; i++) {
+    // Calculate Z offset 
+    let mouse_proximity = calculateMouseProximity(i);
+    let z_off = calculateExhibitZ(mouse_proximity);
+    translate(0, 0, z_off);
+    // Draw cartridge
+    drawCartridge(i);
+    var translate_x = ((i + 1) % row_width != 0) ? entry_width : page_offset;
+    translate(translate_x, 0, 0);
+    // Reset Z
+    translate(0, 0, -1 * z_off);
+  }
+}
+
+function drawCartridge(wave_offset) {
+  const time_ratio = (frameCount % hover_period_in_frames) / hover_period_in_frames;
+  const sin_offset = time_ratio * 2 * PI;
+  translate(0, sin(wave_offset * 10 + sin_offset), 0);
+  specularMaterial(500);
+  fill(95, 95, 120);
+  shininess(3); // 5
+  model(cartridge);
+  translate(0, sin(wave_offset * -10 + sin_offset), 0);
+}
+
+
+
+
+
+
+
---
+++ gallery/gallery_data.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////
+// Application
+const canvas_width = 480;
+const canvas_height = 320;
+const c_grid_state = 0;
+const c_focused_state = 1;
+const project_count = 10;
+
+////////////////////////////////////////////////////////////
+// Resources
+let cartridge;
+
+////////////////////////////////////////////////////////////
+// Effects
+const row_width = 4;
+const page_offset = 128;
+const box_size = 16;
+const box_spacing = 16;
+const hover_y = 180;
+const hover_amplitude = 16;
+const hover_period_in_frames = 300;
+let entry_width, total_width;
+
+////////////////////////////////////////////////////////////
+// Colors
+
+let ambient_color;
+
+////////////////////////////////////////////////////////////
+// Setup function
+
+function preload_gallery() {
+  entry_width = box_size + box_spacing;
+  total_width = (row_width - 1) * entry_width;
+  cartridge = loadModel('../public/models/cartridge.obj');
+  ambient_color = color(202, 253, 253);
+
+}
+
+
+
+
+
+
+
+
+
+
