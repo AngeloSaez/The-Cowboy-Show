c576276d787a9f655930b778f2ec319e167db78f
Angelo Saez <jello3899@gmail.com>
2021-10-15 21:32:35 -0400

    universalized dimensions

--- gallery/gallery_data.js
+++ gallery/gallery_data.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 // Application
-const canvas_width = 480;
-const canvas_height = 320;
+let canvas_width; // 480
+let canvas_height; // 320
 const c_grid_state = 0;
 const c_focused_state = 1;
 const project_count = 10;
@@ -13,26 +13,35 @@ let cartridge;
 ////////////////////////////////////////////////////////////
 // Effects
 const row_width = 4;
-const page_offset = 128;
-const box_size = 16;
-const box_spacing = 16;
-const hover_y = 180;
-const hover_amplitude = 16;
+let page_offset; // 128
+let box_size; // 16
+let box_spacing; // 16
+let hover_y; // 180
+let hover_amplitude; // 16
 const hover_period_in_frames = 300;
 let entry_width, total_width;
 
 ////////////////////////////////////////////////////////////
 // Colors
-
 let ambient_color;
 
 ////////////////////////////////////////////////////////////
 // Setup function
 
 function preload_gallery() {
+  // Application preload
+  canvas_width = displayWidth;
+  canvas_height = displayHeight;
+  // Resources preload
+  cartridge = loadModel('models/cartridge.obj');
+  // Effects preload
+  page_offset = canvas_width / 3;
+  box_size = canvasWidth / 30;
+  box_spacing = box_size;
+  hover_y = canvas_height / 2;
+  hover_amplitude = box_size;
   entry_width = box_size + box_spacing;
   total_width = (row_width - 1) * entry_width;
-  cartridge = loadModel('models/cartridge.obj');
   ambient_color = color(202, 253, 253);
 
 }
