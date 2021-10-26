542673369f591f67a1b81d08921efd8f978ca855
Angelo Saez <jello3899@gmail.com>
2021-10-26 01:51:32 -0400

    fixed cartridge size

--- gallery/gallery_data.js
+++ gallery/gallery_data.js
@@ -36,7 +36,7 @@ function preload_gallery() {
   cartridge = loadModel('models/cartridge.obj');
   // Effects preload
   page_offset = canvas_width / 3;
-  box_size = canvas_height / 10;
+  box_size = canvas_height / 5;
   box_spacing = box_size;
   hover_y = canvas_height / 2;
   hover_amplitude = box_size;
