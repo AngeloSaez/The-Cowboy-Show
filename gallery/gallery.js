d4c17e70eb53df7e672786cc76f31155223de7f5
Angelo Saez <jello3899@gmail.com>
2021-10-27 19:54:30 -0400

    updated sticker position

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -66,11 +66,12 @@ function drawCartridge(wave_offset) {
   scale(box_scale);
   model(cartridge);
   // sticker
-  const sticker_yoff = 4;
-  translate(0, sticker_yoff, 0);
+  const sticker_yoff = 0;
+  const sitcker_zoff = 3;
+  translate(0, sticker_yoff, sitcker_zoff);
   texture(sticker_image_default);
-  plane(box_scale * 2, box_scale * 0.733 * 2);
-  translate(0, -sticker_yoff, 0);
+  plane(box_scale, box_scale * 0.733);
+  translate(0, -sticker_yoff, -sitcker_zoff);
   // reset transform
   translate(-translation_x, -translation_y, 0);
   scale(1.0);
