9bed9e90e8d80efda9d16b83e4860947049d32ca
Angelo Saez <jello3899@gmail.com>
2021-10-27 21:29:02 -0400

    changed material

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -48,7 +48,7 @@ function drawExhibit(wave_offset) {
   // calculate y position and translate
   const time_ratio = (frameCount % hover_period_in_frames) / hover_period_in_frames;
   const sin_offset = time_ratio * 2 * PI;
-  let y_off = sin(wave_offset * 10 + sin_offset) * 0.25; // 10 frame offset, other float is scale
+  let y_off = sin(wave_offset * 10 + sin_offset) * 0.125; // 10 frame offset, other float is scale
   translate(0, y_off, 0);
   // create models
   drawCartridge(y_off);
@@ -70,7 +70,7 @@ function drawCartridge(y_off) {
 function drawSticker() {
   // material setup
   specularMaterial(500);
-  shininess(15);
+  shininess(1);
   // draw
   const p5js_scale = 2.0;
   const height = 0.402 * p5js_scale;
